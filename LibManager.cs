23fbf477e5904814d2b2aadfdd092837db6c14cc
Jake Sheku Kanneh <sheku.kanneh12@gmail.com>
2016-04-12 19:14:51 +1000

    Shortened Code
    Removed obsolete code and replaced with shorter more easy to read code
    that did the same thing.

--- LibManager.cs
+++ LibManager.cs
@@ -4,50 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using TagLib;
 
 namespace Imagine_Music_Player
 {
     class LibManager
     {
+        private string dir_url;
+        public String DIR_URL
+        {
+            set { this.dir_url = value;}
+            get { return this.dir_url; }
+        }
+
         public List<TagLib.File> GetMusic(string dir)
         {
             List<TagLib.File> songs = new List<TagLib.File>();
-            foreach (string s in Directory.GetFiles(dir))
+            foreach (string s in Directory.EnumerateFiles(dir, "*.mp3", SearchOption.AllDirectories))
             {
-                if (s.EndsWith(".mp3") || s.EndsWith(".m4a") || s.EndsWith(".wav"))
-                {
+                if(!s.Contains("MACOSX")) {
                     songs.Add(TagLib.File.Create(s));
+                }
+            }
 
-                }
-                else
-                {
-                    // do nothing
-                }
-            }
-            foreach (string d in Directory.GetDirectories(dir))
-            {
-                try
-                {
-                    if (!d.Contains("MACOSX"))
-                    {
-                        songs.AddRange(GetMusic(d));
-                    }
-                    else
-                    {
-                        //skip file
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            }
+            songs.OrderBy(x => x.Tag.Title);
+            //songs.Sort((x, y) => String.Compare(x.Tag.Title, y.Tag.Title));
+
             return songs;
         }
 
         public String GetTrack(string dir)
         {
             return dir;
+        }
+
+         public TagLib.File[] Songs
+        {
+            get { return GetMusic(dir_url).ToArray(); }
         }
     }
 }
